70508bb61b2dd93b6f6ed7116c8b220bee87c429
Sergei Nezhinskikh <delletenebre@gmail.com>
2021-05-17 00:35:02 +0600

    add mixes

---
+++ lib/api/models/mixes.dart
@@ -0,0 +1,24 @@
+class Mix {
+  // data class Entity(
+  //       @Json(name = "data") val data: Data,
+  //       @Json(name = "id") val id: String,
+  //       @Json(name = "type") val type: String
+  //   ) {
+  //       @JsonClass(generateAdapter = true)
+  //       data class Data(
+  //           @Json(name = "backgroundImageUri") val backgroundImageUri: String,
+  //           @Json(name = "title") val title: String,
+  //           @Json(name = "url") val url: String,
+  //           @Json(name = "urlScheme") val urlScheme: String
+  //       )
+  //   }
+
+  //   Mix.fromJson(Map<String, dynamic> json)
+  //     : name = json['name'],
+  //       email = json['email'];
+
+  //   Map<String, dynamic> toJson() => {
+  //         'name': name,
+  //         'email': email,
+  //       };
+}
